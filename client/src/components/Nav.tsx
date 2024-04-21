4c3084b37b6d470db2b3945a4a5b9ce645ed9824
tantrungnguyen2001 <trungnt@zigvy.com>
2024-04-21 18:38:05 +0700

    update code - navbar

--- client/src/components/Nav.tsx
+++ client/src/components/Nav.tsx
@@ -1,10 +1,4 @@
-import {
-  Button,
-  Navbar,
-  NavbarCollapse,
-  NavbarToggle,
-  TextInput,
-} from "flowbite-react";
+import { Button, Navbar, TextInput } from "flowbite-react";
 import { CiSearch } from "react-icons/ci";
 import { IoIosMoon } from "react-icons/io";
 import { IoLibrary } from "react-icons/io5";
@@ -14,17 +8,19 @@ const Nav = () => {
   const { pathname } = useLocation();
 
   return (
-    <Navbar className="bg-black">
+    <Navbar className="bg-black border-b-2">
       {/* Logo */}
-      <Link
-        to="/"
-        className="self-center whitespace-nowrap flex items-center gap-1"
-      >
-        <div className=" px-3 py-2 bg-gradient-to-r from-indigo-500 via-blue-500 to-pink-500 rounded-xl grid place-items-center text-xl">
-          <IoLibrary />
-        </div>
-        <span className="text-2xl italic font-semibold">MyBlog</span>
-      </Link>
+      <Navbar.Brand as={"div"}>
+        <Link
+          to="/"
+          className="self-center whitespace-nowrap flex items-center gap-1 text-2xl"
+        >
+          <div className=" px-3 py-1 bg-gradient-to-r from-indigo-500 via-blue-500 to-pink-500 rounded-xl grid place-items-center ">
+            <IoLibrary />
+          </div>
+          <span className="italic font-semibold">MyBlog</span>
+        </Link>
+      </Navbar.Brand>
 
       {/* Search */}
       <form>
@@ -33,24 +29,35 @@ const Nav = () => {
           type="text"
           placeholder="Search..."
           rightIcon={CiSearch}
+          className="hide lg:inline"
         />
       </form>
 
-      <div className="flex items-center gap-2">
+      <div className="flex items-center gap-2 md:order-2">
+        <Button pill color={"dark"} className="lg:hidden">
+          <CiSearch />
+        </Button>
         <Button color="dark" pill>
           <IoIosMoon />
         </Button>
-        <NavbarToggle />
+        <Button gradientDuoTone="purpleToBlue" outline>
+          <Link to="sign-in">Sign In</Link>
+        </Button>
+        <Navbar.Toggle />
       </div>
 
-      <NavbarCollapse>
-        <Navbar.Link as={"div"} active={pathname === ""}>
-          <Link to="/">Home</Link>
+      <Navbar.Collapse className="order-1">
+        <Navbar.Link as={"div"} active={pathname === "/"}>
+          <Link to="/" className="block">
+            Home
+          </Link>
         </Navbar.Link>
-        <Navbar.Link as={"div"} active={pathname === "about"}>
-          <Link to="about">About</Link>
+        <Navbar.Link as={"div"} active={pathname === "/about"}>
+          <Link to="about" className="block">
+            About
+          </Link>
         </Navbar.Link>
-      </NavbarCollapse>
+      </Navbar.Collapse>
     </Navbar>
   );
 };
