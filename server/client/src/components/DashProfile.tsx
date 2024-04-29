a3987c34eedce29dff38c434220bb4e18ad2e923
tantrungnguyen2001 <trungnt@zigvy.com>
2024-04-29 16:51:29 +0700

    update user func page

--- server/client/src/components/DashProfile.tsx
+++ server/client/src/components/DashProfile.tsx
@@ -1,4 +1,4 @@
-import { useSelector } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
 import { RootState } from "../services/store";
 import { Alert, Button, Label, TextInput } from "flowbite-react";
 import { useEffect, useRef, useState } from "react";
@@ -11,10 +11,16 @@ import {
 import { app } from "../firebase";
 import { CircularProgressbar } from "react-circular-progressbar";
 import "react-circular-progressbar/dist/styles.css";
+import {
+  updateFailure,
+  updateStart,
+  updateSuccess,
+} from "../services/user/user.slice";
 
 const DashProfile = () => {
   const { currentUser } = useSelector((state: RootState) => state.user);
 
+  // IMAGE
   const [imageFile, setImageFile] = useState<File | null>(null);
   const [imageFileUrl, setImageFileUrl] = useState<string | null>(null);
   const imageRef = useRef() as React.MutableRefObject<HTMLInputElement>;
@@ -24,6 +30,13 @@ const DashProfile = () => {
   const [imageFileUploadError, setImageFileUploadError] = useState<
     string | null
   >(null);
+  const [imageFileUploading, setImageFileUploading] = useState(false);
+
+  // FORMDATA
+  const [formData, setFormData] = useState({});
+  const [updateUserSuccess, setUpdateUserSuccess] = useState("");
+  const [updateUserError, setUpdateUserError] = useState("");
+  const dispatch = useDispatch();
 
   const handleImageFile = (e: React.ChangeEvent<HTMLInputElement>) => {
     if (e.target.files) {
@@ -38,6 +51,7 @@ const DashProfile = () => {
   }, [imageFile]);
 
   const uploadImage = async () => {
+    setImageFileUploading(true);
     setImageFileUploadError(null);
     const storage = getStorage(app);
     if (imageFile) {
@@ -57,20 +71,64 @@ const DashProfile = () => {
           setImageFileUploadingProgress(null);
           setImageFile(null);
           setImageFileUrl(null);
+          setImageFileUploading(false);
         },
         () => {
           getDownloadURL(uploadTask.snapshot.ref).then((downloadUrl) => {
             setImageFileUrl(downloadUrl);
+            setFormData({ ...formData, profilePicture: downloadUrl });
+            setImageFileUploading(false);
           });
         }
       );
     }
   };
 
+  const handleOnChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setFormData({ ...formData, [e.target.id]: e.target.value });
+  };
+
+  const handleSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+
+    setUpdateUserError("");
+    setUpdateUserSuccess("");
+
+    if (Object.keys(formData).length === 0) {
+      setUpdateUserError("No changes made");
+      return;
+    }
+
+    if (imageFileUploading) {
+      setUpdateUserError("Please wait for image to upload");
+      return;
+    }
+    try {
+      dispatch(updateStart());
+      const res = await fetch(`/api/user/update/${currentUser._id}`, {
+        method: "PUT",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(formData),
+      });
+      const data = await res.json();
+      if (!res.ok) {
+        dispatch(updateFailure(data.message));
+        setUpdateUserError(data.message);
+      } else {
+        dispatch(updateSuccess(data));
+        setUpdateUserSuccess(`User's profile updated successfully`);
+      }
+    } catch (error: any) {
+      dispatch(updateFailure(error.message));
+    }
+  };
+
   return (
     <div className="mt-10 space-y-8">
       <h1 className="text-center font-bold text-4xl uppercase">Profile</h1>
-      <form className="max-w-[640px] mx-auto px-4">
+      <form onSubmit={handleSubmit} className="max-w-[640px] mx-auto px-4">
         <input
           type="file"
           accept="image/*"
@@ -123,17 +181,32 @@ const DashProfile = () => {
         <div className="flex flex-col gap-4">
           <div>
             <Label>Username</Label>
-            <TextInput type="text" defaultValue={currentUser.username} />
+            <TextInput
+              type="text"
+              id="username"
+              defaultValue={currentUser.username}
+              onChange={handleOnChange}
+            />
           </div>
           <div>
             <Label>Email</Label>
-            <TextInput type="text" defaultValue={currentUser.email} />
+            <TextInput
+              type="text"
+              id="email"
+              defaultValue={currentUser.email}
+              onChange={handleOnChange}
+            />
           </div>
           <div>
             <Label>Password</Label>
-            <TextInput type="text" placeholder="*******" />
+            <TextInput
+              type="text"
+              placeholder="*******"
+              id="password"
+              onChange={handleOnChange}
+            />
           </div>
-          <Button pill gradientDuoTone="purpleToBlue" outline>
+          <Button type="submit" pill gradientDuoTone="purpleToBlue" outline>
             Update
           </Button>
           <div className="flex items-center justify-between text-red-500 text-sm">
@@ -141,6 +214,17 @@ const DashProfile = () => {
             <p className="cursor-pointer">Sign out</p>
           </div>
         </div>
+
+        {updateUserSuccess && (
+          <Alert color="success" className="mt-5">
+            {updateUserSuccess}
+          </Alert>
+        )}
+        {updateUserError && (
+          <Alert color="failure" className="mt-5">
+            {updateUserError}
+          </Alert>
+        )}
       </form>
     </div>
   );
--- server/client/src/services/user/user.slice.tsx
+++ server/client/src/services/user/user.slice.tsx
@@ -2,7 +2,7 @@ import { createSlice } from "@reduxjs/toolkit";
 import { userTypes } from "../../types/userTypes";
 
 type initialStateType = {
-  currentUser: Omit<userTypes, "_id">;
+  currentUser: userTypes;
   isLoading: boolean;
   error: string | null;
 };
@@ -35,9 +35,29 @@ export const userSlice = createSlice({
       state.isLoading = false;
       state.error = action.payload;
     },
+    updateStart: (state) => {
+      state.isLoading = true;
+      state.error = null;
+    },
+    updateSuccess: (state, action) => {
+      state.currentUser = action.payload;
+      state.isLoading = false;
+      state.error = null;
+    },
+    updateFailure: (state, action) => {
+      state.isLoading = false;
+      state.error = action.payload;
+    },
   },
 });
 
-export const { signInStart, signInSuccess, signInFailure } = userSlice.actions;
+export const {
+  signInStart,
+  signInSuccess,
+  signInFailure,
+  updateStart,
+  updateSuccess,
+  updateFailure,
+} = userSlice.actions;
 
 export default userSlice.reducer;
