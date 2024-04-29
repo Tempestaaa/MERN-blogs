eda118f08f97a95a1d6b64233620c87a9b0fbcdf
tantrungnguyen2001 <trungnt@zigvy.com>
2024-04-29 16:59:21 +0700

    delete api route

--- server/routes/user.router.js
+++ server/routes/user.router.js
@@ -1,9 +1,10 @@
 import express from "express";
 import verifiedUser from "../utils/verifiedUser.js";
-import { updateUser } from "../controllers/user.controller.js";
+import { updateUser, deleteUser } from "../controllers/user.controller.js";
 
 const userRouter = express.Router();
 
 userRouter.put("/update/:userId", verifiedUser, updateUser);
+userRouter.delete("/delete/:userId", verifiedUser, deleteUser);
 
 export default userRouter;
