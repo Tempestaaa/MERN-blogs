2f3a29c1156eb2d75e7d17715e384ffe14bb8040
tantrungnguyen2001 <trungnt@zigvy.com>
2024-05-03 05:01:02 +0700

    create get users api route

--- server/routes/user.router.js
+++ server/routes/user.router.js
@@ -4,6 +4,7 @@ import {
   updateUser,
   deleteUser,
   signOut,
+  getUsers,
 } from "../controllers/user.controller.js";
 
 const userRouter = express.Router();
@@ -11,5 +12,6 @@ const userRouter = express.Router();
 userRouter.put("/update/:userId", verifiedUser, updateUser);
 userRouter.delete("/delete/:userId", verifiedUser, deleteUser);
 userRouter.post("/signout", signOut);
+userRouter.get("/getusers", verifiedUser, getUsers);
 
 export default userRouter;
