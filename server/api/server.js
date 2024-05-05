7c65ca2780104f11650fcc83c9086ea653cc7c1a
tantrungnguyen2001 <trungnt@zigvy.com>
2024-05-06 06:04:34 +0700

    pre installation

--- server/api/server.js
+++ server/api/server.js
@@ -2,11 +2,12 @@ import express from "express";
 import dotenv from "dotenv";
 import cors from "cors";
 import mongoose from "mongoose";
-import blogRouter from "../routes/blog.router.js";
-import authRouter from "../routes/auth.router.js";
-import userRouter from "../routes/user.router.js";
-import commentRouter from "../routes/comment.router.js";
 import cookieParser from "cookie-parser";
+import blogRouter from "./routes/blog.router.js";
+import authRouter from "./routes/auth.router.js";
+import userRouter from "./routes/user.router.js";
+import commentRouter from "./routes/comment.router.js";
+import path from "path";
 
 dotenv.config();
 const app = express();
@@ -16,10 +17,18 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 const PORT = process.env.PORT | 8000;
 
+const __dirname = path.resolve();
+
 app.use("/api/blog", blogRouter);
 app.use("/api/auth", authRouter);
 app.use("/api/user", userRouter);
 app.use("/api/comment", commentRouter);
+
+app.use(express.static(path.join(__dirname, "/client/dist")));
+
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "client", "dist", "index.html"));
+});
 
 app.use((err, req, res, next) => {
   const statusCode = err.statusCode || 500;
